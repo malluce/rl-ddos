0a6d9262442acb4334e1dcebc374c7ec8e0f8fb4
Felix Bachmann <felix.bachmann2@student.kit.edu>
2021-06-17 08:29:46 +0200

    smaller DQN arch, added playground for experimenting

--- agents/dqn_playground.py
+++ agents/dqn_playground.py
@@ -10,6 +10,7 @@ from __future__ import print_function
 import os
 import time
 
+import tf_agents.drivers.dynamic_episode_driver
 from absl import app
 from absl import flags
 from absl import logging
@@ -83,21 +84,21 @@ def train_eval(
         summarize_grads_and_vars=False,
         state_selection=['base', 'actions', 'distvol', 'fpr', 'distvolstd', 'bldist'],
         actionset_selection='LargeDiscreteActionSet',
-        trace_length=500):
+        trace_length=10):
     """A simple train and eval for DQN."""
+
     if timestamp is None:
         timestamp = Datastore.get_timestamp()
+
+    # setup writers
     root_dir = os.path.expanduser(root_dir)
     root_dir = os.path.join(root_dir, timestamp)
-    train_dir = os.path.join(root_dir, 'train')
-    eval_dir = os.path.join(root_dir, 'eval')
-
     train_summary_writer = tf.compat.v2.summary.create_file_writer(
-        train_dir, flush_millis=summaries_flush_secs * 1000)
-
+        os.path.join(root_dir, 'train'), flush_millis=summaries_flush_secs * 1000)
     eval_summary_writer = tf.compat.v2.summary.create_file_writer(
-        eval_dir, flush_millis=summaries_flush_secs * 1000)
+        os.path.join(root_dir, 'eval'), flush_millis=summaries_flush_secs * 1000)
 
+    # setup metrics
     train_metrics = [
         tf_metrics.NumberOfEpisodes(),
         tf_metrics.EnvironmentSteps(),
@@ -111,148 +112,138 @@ def train_eval(
         #		tf_metrics.ChosenActionHistogram()
     ]
 
+    # setup global step counter
     global_step = tf.compat.v1.train.get_or_create_global_step()
 
-    with train_summary_writer.as_default():
-        ds_train = Datastore('train', timestamp)
-        ds_eval = Datastore('eval', timestamp)
+    # setup datastore
+    ds_train = Datastore('train', timestamp)
+    ds_eval = Datastore('eval', timestamp)
 
-        gym_kwargs = {
-            'state_selection': state_selection,
-            'actionset_selection': actionset_selection,
-            'trace_length': trace_length
-        }
+    # setup envs (train, eval)
+    gym_kwargs = {
+        'state_selection': state_selection,
+        'actionset_selection': actionset_selection,
+        'trace_length': trace_length
+    }
+    train_gym = suite_gym.load(
+        env_name, gym_kwargs={'data_store': ds_train, **gym_kwargs},
+        spec_dtype_map={gym.spaces.Discrete: np.int32})
+    tf_env = tf_py_environment.TFPyEnvironment(train_gym)
+    eval_tf_env = tf_py_environment.TFPyEnvironment(suite_gym.load(
+        env_name, gym_kwargs={'data_store': ds_eval, **gym_kwargs},
+        spec_dtype_map={gym.spaces.Discrete: np.int32}))
 
-        logging.info('creating train gym...')
-        train_gym = suite_gym.load(
-            env_name, gym_kwargs={'data_store': ds_train, **gym_kwargs},
-            spec_dtype_map={gym.spaces.Discrete: np.int32})
-        logging.info('casting to tf env')
-        tf_env = tf_py_environment.TFPyEnvironment(train_gym)
-        logging.info('creating eval env...')
-        eval_tf_env = tf_py_environment.TFPyEnvironment(suite_gym.load(
-            env_name, gym_kwargs={'data_store': ds_eval, **gym_kwargs},
-            spec_dtype_map={gym.spaces.Discrete: np.int32}))
+    # setup DQN
+    q_net = q_network.QNetwork(
+        tf_env.observation_spec(),
+        tf_env.action_spec(),
+        fc_layer_params=fc_layer_params)
+    train_sequence_length = n_step_update
 
-        if train_sequence_length != 1 and n_step_update != 1:
-            raise NotImplementedError(
-                'train_eval does not currently support n-step updates with stateful '
-                'networks (i.e., RNNs)')
+    # setup agent
+    # TODO(b/127301657): Decay epsilon based on global step, cf. cl/188907839
+    tf_agent = dqn_agent.DqnAgent(
+        tf_env.time_step_spec(),
+        tf_env.action_spec(),
+        q_network=q_net,
+        epsilon_greedy=epsilon_greedy,
+        n_step_update=n_step_update,
+        target_update_tau=target_update_tau,
+        target_update_period=target_update_period,
+        #			optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate = learning_rate),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+        td_errors_loss_fn=common.element_wise_squared_loss,
+        gamma=gamma,
+        reward_scale_factor=reward_scale_factor,
+        gradient_clipping=gradient_clipping,
+        debug_summaries=debug_summaries,
+        summarize_grads_and_vars=summarize_grads_and_vars,
+        train_step_counter=global_step)
+    tf_agent.initialize()
 
-        logging.info('Using QNetwork')
-        q_net = q_network.QNetwork(
-            tf_env.observation_spec(),
-            tf_env.action_spec(),
-            fc_layer_params=fc_layer_params)
-        train_sequence_length = n_step_update
+    eval_policy = tf_agent.policy
+    collect_policy = tf_agent.collect_policy
 
-        # TODO(b/127301657): Decay epsilon based on global step, cf. cl/188907839
-        tf_agent = dqn_agent.DqnAgent(
-            tf_env.time_step_spec(),
-            tf_env.action_spec(),
-            q_network=q_net,
-            epsilon_greedy=epsilon_greedy,
-            n_step_update=n_step_update,
-            target_update_tau=target_update_tau,
-            target_update_period=target_update_period,
-            #			optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate = learning_rate),
-            optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
-            td_errors_loss_fn=common.element_wise_squared_loss,
-            gamma=gamma,
-            reward_scale_factor=reward_scale_factor,
-            gradient_clipping=gradient_clipping,
-            debug_summaries=debug_summaries,
-            summarize_grads_and_vars=summarize_grads_and_vars,
-            train_step_counter=global_step)
-        tf_agent.initialize()
+    replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+        data_spec=tf_agent.collect_data_spec,
+        batch_size=tf_env.batch_size,
+        max_length=replay_buffer_capacity)
+    collect_driver = dynamic_step_driver.DynamicStepDriver(
+        tf_env,
+        collect_policy,
+        observers=[replay_buffer.add_batch] + train_metrics,
+        num_steps=collect_steps_per_iteration)
 
-        eval_policy = tf_agent.policy
-        collect_policy = tf_agent.collect_policy
+    if use_tf_functions:
+        collect_driver.run = common.function(collect_driver.run)
+        tf_agent.train = common.function(tf_agent.train)
 
-        replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
-            data_spec=tf_agent.collect_data_spec,
-            batch_size=tf_env.batch_size,
-            max_length=replay_buffer_capacity)
-        collect_driver = dynamic_step_driver.DynamicStepDriver(
-            tf_env,
-            collect_policy,
-            observers=[replay_buffer.add_batch] + train_metrics,
-            num_steps=collect_steps_per_iteration)
+    ds_eval.commit_config(gin.operative_config_str())
 
-        if use_tf_functions:
-            collect_driver.run = common.function(collect_driver.run)
-            tf_agent.train = common.function(tf_agent.train)
+    logging.info('Initializing replay buffer')
+    dynamic_step_driver.DynamicStepDriver(
+        tf_env,
+        random_tf_policy.RandomTFPolicy(
+            tf_env.time_step_spec(), tf_env.action_spec()),
+        observers=[replay_buffer.add_batch] + train_metrics,
+        num_steps=initial_collect_steps).run()
 
-        initial_collect_policy = random_tf_policy.RandomTFPolicy(
-            tf_env.time_step_spec(), tf_env.action_spec())
+    time_step = None
+    policy_state = collect_policy.get_initial_state(tf_env.batch_size)
+    timed_at_step = global_step.numpy()
+    time_acc = 0
 
-        ds_eval.commit_config(gin.operative_config_str())
+    # Dataset generates trajectories with shape [Bx2x...]
+    dataset = replay_buffer.as_dataset(
+        num_parallel_calls=3,
+        sample_batch_size=batch_size,
+        num_steps=train_sequence_length + 1).prefetch(3)
+    iterator = iter(dataset)
 
-        logging.info('Initializing replay buffer')
+    def train_step():
+        experience, _ = next(iterator)
+        return tf_agent.train(experience)
 
-        dynamic_step_driver.DynamicStepDriver(
-            tf_env,
-            initial_collect_policy,
-            observers=[replay_buffer.add_batch] + train_metrics,
-            num_steps=initial_collect_steps).run()
+    if use_tf_functions:
+        train_step = common.function(train_step)
 
-        time_step = None
-        policy_state = collect_policy.get_initial_state(tf_env.batch_size)
+    logging.info('Training')
 
-        timed_at_step = global_step.numpy()
-        time_acc = 0
+    for _ in range(num_iterations):
+        logging.info(f"==== Starting step {_} ====")
+        start_time = time.time()
+        time_step, policy_state = collect_driver.run(
+            time_step=time_step,
+            policy_state=policy_state,
+        )
+        print(time_step)
+        for _ in range(train_steps_per_iteration):
+            train_loss = train_step()
+        time_acc += time.time() - start_time
 
-        # Dataset generates trajectories with shape [Bx2x...]
-        dataset = replay_buffer.as_dataset(
-            num_parallel_calls=3,
-            sample_batch_size=batch_size,
-            num_steps=train_sequence_length + 1).prefetch(3)
-        iterator = iter(dataset)
-
-        def train_step():
-            experience, _ = next(iterator)
-            return tf_agent.train(experience)
-
-        if use_tf_functions:
-            train_step = common.function(train_step)
-
-        logging.info('Training')
-
-        for _ in range(num_iterations):
-            start_time = time.time()
-            time_step, policy_state = collect_driver.run(
-                time_step=time_step,
-                policy_state=policy_state,
-            )
-            for _ in range(train_steps_per_iteration):
-                train_loss = train_step()
-            time_acc += time.time() - start_time
-
-            if global_step.numpy() % log_interval == 0:
-                logging.info('step = %d, loss = %f', global_step.numpy(),
-                             train_loss.loss)
-                steps_per_sec = (global_step.numpy() - timed_at_step) / time_acc
-                logging.info('%.3f steps/sec', steps_per_sec)
+        if global_step.numpy() % log_interval == 0:
+            logging.info('step = %d, loss = %f', global_step.numpy(),
+                         train_loss.loss)
+            steps_per_sec = (global_step.numpy() - timed_at_step) / time_acc
+            logging.info('%.3f steps/sec', steps_per_sec)
+            with train_summary_writer.as_default():
                 tf.compat.v2.summary.scalar(name='global_steps_per_sec',
                                             data=steps_per_sec, step=global_step)
-                timed_at_step = global_step.numpy()
-                time_acc = 0
+            timed_at_step = global_step.numpy()
+            time_acc = 0
 
-            if global_step.numpy() % eval_interval == 0:
-                results = metric_utils.eager_compute(
-                    eval_metrics,
-                    eval_tf_env,
-                    eval_policy,
-                    num_episodes=num_eval_episodes,
-                    train_step=global_step,
-                    summary_writer=eval_summary_writer,
-                    summary_prefix='Metrics',
-                    use_function=use_tf_functions
-                )
-
-                metric_utils.log_metrics(eval_metrics)
-
-        return train_loss
+        if global_step.numpy() % eval_interval == 0:
+            metric_utils.eager_compute(
+                eval_metrics,
+                eval_tf_env,
+                eval_policy,
+                num_episodes=num_eval_episodes,
+                train_step=global_step,
+                summary_writer=eval_summary_writer,
+                summary_prefix='Metrics',
+                use_function=use_tf_functions
+            )
+            metric_utils.log_metrics(eval_metrics)
 
 
 def main(_):
