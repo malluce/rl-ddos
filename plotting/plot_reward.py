6c1f24311bf1c1b61e7e498a101fec410a51bcce
Felix Bachmann <felix.bachmann2@student.kit.edu>
2021-07-10 17:18:35 +0200

    can now plot over all episodes and in-episode behavior

--- plotting/plot_reward.py
+++ plotting/plot_reward.py
@@ -1,47 +1,76 @@
+import os
 import matplotlib.pyplot as plt
 import pandas
 
 
-def plot_results(episode_file, label):
-    episodes = pandas.read_csv(episode_file)
-    rewards = episodes.iloc[:, -1]
-    precisions = episodes.iloc[:, 3]
-    recalls = episodes.iloc[:, 4]
-    rules = episodes.iloc[:, 2]
-    fig: plt.Figure = plt.figure()
+def plot_episode_behavior(environment_file_path, last_x_episodes: int = None, show_in_episode_behaviour: bool = False):
+    """
+    Plots the metrics of an environment.csv file.
+    :param environment_file_path: the environment.csv file path
+    :param last_x_episodes: the number of episodes to plot (taken from the end), only relevant if show_in_episode_behavior is True
+    :param show_in_episode_behaviour: whether to show the steps inside the episodes on the x axis
+    """
 
-    rew_ax = fig.add_subplot(2, 2, 1)
-    rew_ax.plot(range(rewards.shape[0]), rewards, label=label)
-    rew_ax.set_xlabel('episodes')
-    rew_ax.set_ylabel('reward')
-    rew_ax.set_ylim(bottom=0)
-    rew_ax.legend()
+    if not ((last_x_episodes is not None and show_in_episode_behaviour) or (
+            last_x_episodes is None and not show_in_episode_behaviour)):
+        print('Not currently supported, aborting')
+        return 1
 
-    prec_ax = fig.add_subplot(2, 2, 2)
-    prec_ax.plot(range(precisions.shape[0]), precisions, label=label)
-    prec_ax.set_xlabel('episodes')
-    prec_ax.set_ylabel('precision')
-    prec_ax.set_ylim(bottom=0, top=1)
-    prec_ax.legend()
+    env_csv = pandas.read_csv(environment_file_path)
+    cols = list(map(lambda x: str(x).strip(), list(env_csv.columns)))  # strip spaces from col names
+    env_csv = pandas.read_csv(environment_file_path, header=0, names=cols)  # read csv again with stripped col names
 
-    rec_ax = fig.add_subplot(2, 2, 3)
-    rec_ax.plot(range(recalls.shape[0]), recalls, label=label)
-    rec_ax.set_xlabel('episodes')
-    rec_ax.set_ylabel('recall')
-    rec_ax.set_ylim(bottom=0, top=1)
-    rec_ax.legend()
+    if show_in_episode_behaviour:  # show steps on x-axis, for last_x_episodes only
+        def get_rows_with_episode_in(episodes):
+            filtered_rows = env_csv.loc[env_csv['Episode'].isin(episodes), :]  # get rows where episode in episodes
+            return filtered_rows.groupby(['Step']).mean(), filtered_rows.groupby(
+                ['Step']).std()  # return mean and std of all columns for each step
 
-    rules_ax = fig.add_subplot(2, 2, 4)
-    rules_ax.plot(range(rules.shape[0]), rules, label=label)
-    rules_ax.set_xlabel('episodes')
-    rules_ax.set_ylabel('number of rules')
-    rules_ax.set_ylim(bottom=0)
-    rules_ax.legend()
+        eps = env_csv.loc[:, 'Episode']
+        unique_eps = sorted(list(set(eps)))  # get all unique episode numbers (some are not included in csv files)
+        eps_to_show = unique_eps[len(unique_eps) - last_x_episodes:]
+        last, last_std = get_rows_with_episode_in(eps_to_show)
+    else:  # show all episodes on x-axis
+        grouped_by_episode = env_csv.groupby(['Episode'])
+        last, last_std = grouped_by_episode.mean(), grouped_by_episode.std()
 
-    plt.tight_layout()
+    def plot(fig: plt.Figure, data_frame, data_frame_std, cols, x, is_one_bounded=False):
+        ax: plt.Axes = fig.add_subplot(2, 4, x)
+        x = range(data_frame.shape[0])
+        for col in cols:
+            y = data_frame.loc[:, col]
+            y_err = data_frame_std.loc[:, col]
+            ax.plot(x, y, label=col)
+            ax.fill_between(x, y - y_err, y + y_err, alpha=0.2)
+            if show_in_episode_behaviour:
+                ax.set_xlabel('step')
+            else:
+                ax.set_xlabel('episode')
+        if is_one_bounded:
+            ax.set_ylim(bottom=0, top=1.1)
+        else:
+            ax.set_ylim(bottom=0)
+        ax.legend()
+
+    fig: plt.Figure = plt.figure(figsize=(16, 8))
+    plot(fig, last, last_std, ['Phi'], 1, is_one_bounded=True)
+    plot(fig, last, last_std, ['MinPrefix'], 2)
+    plot(fig, last, last_std, ['Precision', 'EstPrecision'], 3, is_one_bounded=True)
+    plot(fig, last, last_std, ['Recall', 'EstRecall'], 4, is_one_bounded=True)
+    plot(fig, last, last_std, ['BlackSize'], 5)
+    plot(fig, last, last_std, ['FPR', 'EstFPR'], 6, is_one_bounded=True)
+    plot(fig, last, last_std, ['Reward'], 7)
+    if last_x_episodes is not None:
+        fig.suptitle(f'last {last_x_episodes} eval episodes')
+    else:
+        fig.suptitle(f'all episodes')
+    fig.tight_layout()
     plt.show()
 
 
 if __name__ == '__main__':
-    episode_file = 'D:\\home\\bachmann\\test-pycharm\\data\\sb-dqn_20210625-090803\\datastore\\train\\episodes.csv'
-    plot_results(episode_file, label='')
+    ds_base = '/home/bachmann/test-pycharm/data/td3_20210710-105917/datastore'
+    train_path = os.path.join(ds_base, 'train', 'environment.csv')
+    eval_path = os.path.join(ds_base, 'eval', 'environment.csv')
+    plot_episode_behavior(environment_file_path=train_path, show_in_episode_behaviour=False)
+    plot_episode_behavior(environment_file_path=eval_path, last_x_episodes=5, show_in_episode_behaviour=True)
