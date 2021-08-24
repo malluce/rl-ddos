c80347ac4401b7e9d2bbf81c40e7d92a77023401
Felix Bachmann <felix.bachmann2@student.kit.edu>
2021-08-24 08:22:19 +0200

    adapted call signature for baseline eval

--- eval/baseline_eval.py
+++ eval/baseline_eval.py
@@ -1,22 +1,21 @@
 import gym
-from .eval_agents import FixedEvalAgent, RandomEvalAgent
+from eval_agents import FixedEvalAgent, RandomEvalAgent
 from gyms.hhh.actionset import ActionSet, DirectResolveActionSet, LargeDiscreteActionSet
 from gyms.hhh.env import register_hhh_gym
 from gyms.hhh.state import BaseObservations, BlocklistDistribution, DistVol, DistVolStd, FalsePositiveRate, \
     MinMaxBlockedAddress
 from lib.datastore import Datastore
 from agents.util import get_dirs
-from plotting.plot_reward import plot_results
+from plotting.plot_results import plot_episode_behavior
 
 env_name = register_hhh_gym()
-TRACE_LEN = 600
 
 
 def eval(eval_agent, action_set, label):
-    dirs = get_dirs('/home/bachmann/test-pycharm/data', Datastore.get_timestamp(), 'sb-dqn')
+    dirs = get_dirs('/home/bachmann/test-pycharm/data', Datastore.get_timestamp(), 'eval-baseline')
     ds_train, env = make_env(dirs, action_set)
     run_eval_episodes(env, eval_agent)
-    plot_results(ds_train.episode_file.name, label=label)
+    plot_episode_behavior(ds_train.environment_file.name)
 
 
 def run_eval_episodes(env, eval_agent, num_episodes=5):
@@ -34,18 +33,17 @@ def make_env(dirs, actionset):
                                 DistVolStd(),
                                 BlocklistDistribution()],
         'use_prev_action_as_obs': False,
-        'actionset': actionset,
-        'trace_length': TRACE_LEN
+        'actionset': actionset
     }
     ds_train = Datastore(dirs['root'], 'train')
     return ds_train, gym.make(env_name, **{'data_store': ds_train, **gym_kwargs})
 
 
-action_set = LargeDiscreteActionSet()
-eval_agent = RandomEvalAgent(action_set.actionspace, action_set)
-eval(eval_agent, action_set, 'random(LargeDiscrete)')
+# action_set = LargeDiscreteActionSet()
+# eval_agent = RandomEvalAgent(action_set.actionspace, action_set)
+# eval(eval_agent, action_set, 'random(LargeDiscrete)')
 
-phi = 0.05
-L = 16
+phi = 0.57
+L = 21
 eval_agent = FixedEvalAgent(phi, L)
 eval(eval_agent, DirectResolveActionSet(), f'$\phi={phi}$, $L={L}$')
