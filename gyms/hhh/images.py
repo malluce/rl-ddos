5b798618336361f7699df9fd38c5e146e40730f7
Felix Bachmann <felix.bachmann2@student.kit.edu>
2021-09-02 18:00:41 +0200

    fixed empty rightmost column; added squashing; made images square)

--- gyms/hhh/images.py
+++ gyms/hhh/images.py
@@ -3,13 +3,18 @@ import numpy as np
 
 from gyms.hhh.label import Label
 
-IMAGE_SIZE = 128  # the image size (width=height) in pixels
 ADDRESS_SPACE = 16  # the size of the address space, hierarchy levels are [ADDRESS_SPACE,32]
+IMAGE_SIZE = 15  # 8  # the image size (pixels, width=height) in multiples of ADDRESS_SPACE+1
+
+# if the maximum pixel value is at least SQUASH_THRESHOLD times larger than the 2nd smallest value, then
+# values will be squashed together (smallest value will (likely) be 0, so take 2nd smallest)
+SQUASH_THRESHOLD = 1
 
 
-def generate_hhh_image(hhh_algo, img_size=IMAGE_SIZE, address_space=ADDRESS_SPACE):
+def generate_hhh_image(hhh_algo, img_size=IMAGE_SIZE, address_space=ADDRESS_SPACE, squash_threshold=SQUASH_THRESHOLD):
     start = time.time()
 
+    img_size_px = img_size * (ADDRESS_SPACE + 1)
     max_addr = 2 ** address_space - 1
 
     # query HHH for all undiscounted counters, filter hierarchy to be in address space
@@ -30,10 +35,10 @@ def generate_hhh_image(hhh_algo, img_size=IMAGE_SIZE, address_space=ADDRESS_SPAC
     split_by_level = np.split(res[:, 1:], np.sort(unique_indices)[1:])[::-1]
 
     # the bounds of IMAGE_SIZE many bins to separate the address space (x-axis)
-    bounds = np.linspace(0, max_addr, num=img_size, dtype=np.int)
+    bounds = np.linspace(0, max_addr, num=img_size_px, dtype=np.int)
 
     # init output image (x-axis=address bins, y-axis=hierarchy levels)
-    image = np.zeros((len(split_by_level), img_size), dtype=np.float64)
+    image = np.zeros((len(split_by_level), img_size_px), dtype=np.float64)
 
     # iterate over all hierarchy levels and the corresponding item lists
     for level, l in enumerate(split_by_level):
@@ -54,26 +59,38 @@ def generate_hhh_image(hhh_algo, img_size=IMAGE_SIZE, address_space=ADDRESS_SPAC
             last_bin = l_end_binned[bin_index] - 1
             if first_bin == last_bin:  # only increment current bin
                 image[level, first_bin] += count
+
+                if last_bin == len(bounds) - 1:  # also increment last bin if on the right edge
+                    image[level, last_bin] += count
             else:  # increment all bins covered by the current item
+                # also increment last bin if on the right edge
+                # otherwise exclude it (i.e. range(first_bin, last_bin)) to avoid duplicate additions of count
+                if last_bin == len(bounds) - 1:
+                    image[level, last_bin] += count
+
                 for bin_to_increment in range(first_bin, last_bin):
                     image[level, bin_to_increment] += count
+
+    # if needed squash distant values together to increase visibility of smaller IP sources
+    second_smallest_value = np.partition(np.unique(image.flatten()), 1)[1]
+    print(f'ratio={np.max(image) / second_smallest_value}')
+    if np.max(image) / second_smallest_value >= squash_threshold:
+        image = np.log(image, where=np.invert(np.isclose(image, 0.0)))
 
     # normalize values to be in  [0,1]
     image = image / np.max(image)
 
     # stretch y-axis to match the x-axis (square image)
-    print(image.shape)
-    image = np.repeat(image, img_size / address_space, axis=0)
-    print(image.shape)
-    print(f'[hhh] query_all and CNN image build time={time.time() - start}')
-    # TODO img=log(img) to increase clarity for distant values? (image = np.log(image, where=image != 0))
-
+    image = np.repeat(image, img_size, axis=0)
+    # TODO possibly apply colormap and expand dims
+    print(f'[hhh] query_all and CNN image (shape {image.shape}) build time={time.time() - start}')
     return image
 
 
 def generate_filter_image(hhh_query_result, img_size=IMAGE_SIZE, address_space=ADDRESS_SPACE):
     start = time.time()
 
+    img_size_px = img_size * (ADDRESS_SPACE + 1)
     max_addr = 2 ** address_space - 1
 
     #
@@ -81,14 +98,13 @@ def generate_filter_image(hhh_query_result, img_size=IMAGE_SIZE, address_space=A
     rules = rules[rules[:, 1] >= address_space, :]
 
     # the bounds of IMAGE_SIZE many bins to separate the address space (x-axis)
-    bounds = np.linspace(0, max_addr, num=img_size, dtype=np.int)
+    bounds = np.linspace(0, max_addr, num=img_size_px, dtype=np.int)
 
     # init output image (x-axis=address bins, y-axis=hierarchy levels)
-    image = np.zeros((address_space + 1, img_size), dtype=np.float64)
+    image = np.zeros((address_space + 1, img_size_px), dtype=np.int)
 
     # iterate over all rules
     for ip, level in rules:
-        print(ip, level)
 
         if level != 32:  # if not at the bottom, a range of addresses is blocked
             subnet_size = Label.subnet_size(level)
@@ -97,21 +113,20 @@ def generate_filter_image(hhh_query_result, img_size=IMAGE_SIZE, address_space=A
             end_address = ip
 
         # set image pixels to 1 for blocked ranges of the address space, 0 (default) otherwise
-        first_bin = np.digitize(ip, bins=bounds)
-        last_bin = np.digitize(end_address, bins=bounds)
+        first_bin = np.digitize(ip, bins=bounds) - 1
+        last_bin = np.digitize(end_address, bins=bounds) - 1
 
         shifted_level = level - address_space
         if first_bin == last_bin:
             image[shifted_level:, first_bin] = 1
         else:
-            for blocked_bin in range(first_bin, last_bin):
+            for blocked_bin in range(first_bin, last_bin + 1):
                 image[shifted_level:, blocked_bin] = 1
 
     # stretch y-axis to match the x-axis (square image)
-    print(image.shape)
-    image = np.repeat(image, img_size / address_space, axis=0)
-    print(image.shape)
-    print(f'[filter] CNN image build time={time.time() - start}')
-    # TODO img=log(img) to increase clarity for distant values? (image = np.log(image, where=image != 0))
+    image = np.repeat(image, img_size, axis=0)
 
+    # output should be (width, height, channels=1)
+    image = np.expand_dims(image, 2)
+    print(f'[filter] CNN image (shape {image.shape}) build time={time.time() - start}')
     return image
