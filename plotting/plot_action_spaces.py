f8086a70b39b84724a95169cf69866963b009efa
Felix Bachmann <felix.bachmann2@student.kit.edu>
2021-12-14 09:02:14 +0100

    added script for plotting action space discretization and resolving

---
+++ plotting/plot_action_spaces.py
@@ -0,0 +1,72 @@
+import sys
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+from gyms.hhh.actionset import EvenSmallerDiscreteRejectionActionSet, EvenSmallerDiscreteRejectionActionSet2, \
+    NotSoHugeDiscreteActionSet, \
+    SmallDiscreteRejectionActionSet
+
+matplotlib.rcParams.update({'font.size': 15})
+
+
+def agent_action_to_resolved_phi(agent_action, lower_bound, upper_bound):
+    return np.maximum((1 / (1 + np.exp(-3.25 * (agent_action - 1)))) * 2 * upper_bound, lower_bound)
+
+
+def agent_action_to_resolved_pthresh(agent_action, lower_bound, upper_bound):
+    middle = (upper_bound + lower_bound) / 2
+    middle_to_bound = np.abs(middle - upper_bound)
+    return middle + middle_to_bound * agent_action
+
+
+print(agent_action_to_resolved_phi(1, 0.001, 0.3))
+
+agent_action = np.linspace(-1.0, 1.0, 1000)
+resolved = agent_action_to_resolved_pthresh(agent_action=agent_action, lower_bound=0.85, upper_bound=1.0)
+print(min(resolved), max(resolved))
+plt.plot(agent_action, resolved, label='pthresh')
+resolved = agent_action_to_resolved_pthresh(agent_action=agent_action, lower_bound=0.001, upper_bound=0.3)
+print(min(resolved), max(resolved))
+# print(resolved)
+plt.plot(agent_action, resolved, label='$\phi$')
+plt.xlabel('Action chosen by agent')
+plt.ylabel('Resolved mitigation parameter')
+# plt.ylim(bottom=0.85, top=1.0)
+# plt.yticks([0, 0.25, 0.5, 0.75, 1.0])
+plt.legend()
+plt.tight_layout()
+plt.show()
+# sys.exit(0)
+
+plt.fill_between([0, 1], [0], [1])
+plt.xlim(left=0.0, right=1.0)
+plt.ylim(bottom=0.0, top=1.0)
+plt.xlabel('$\phi$')
+plt.ylabel('pthresh')
+plt.tight_layout()
+plt.show()
+
+plt.fill_between([0.001, 0.3], [0.85], [1])
+plt.xlim(left=0.0, right=1.0)
+plt.ylim(bottom=0.0, top=1.0)
+plt.yticks([0, 0.25, 0.5, 0.75, 1.0])
+plt.xticks([0, 0.25, 0.5, 0.75, 1.0])
+plt.xlabel('$\phi$')
+plt.ylabel('pthresh')
+plt.tight_layout()
+plt.show()
+
+a = NotSoHugeDiscreteActionSet()
+print(len(a.actions))
+a = EvenSmallerDiscreteRejectionActionSet2()
+x = list(map(lambda act: act[0], a.actions))
+y = list(map(lambda act: act[1], a.actions))
+plt.scatter(x, y, linewidths=1, marker='x')
+plt.xlabel('$\phi$')
+plt.yticks([0.85, 0.9, 0.95, 1.0])
+plt.ylabel('pthresh')
+# plt.xscale('log')
+plt.tight_layout()
+plt.show()
