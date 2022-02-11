5991667350fd288137cb18c16426a5b31be2b815
Felix Bachmann <felix.bachmann2@student.kit.edu>
2022-02-11 13:42:26 +0100

    fixed relative includes for gin files, removed unneccesary external gin configurables

--- training/runner.py
+++ training/runner.py
@@ -3,6 +3,7 @@ import tf_agents
 from absl import app, flags, logging
 import tensorflow
 import dask
+import pathlib
 from gyms.hhh.env import register_hhh_gym
 from training.train_loop import get_train_loop
 
@@ -11,6 +12,10 @@ FLAGS = flags.FLAGS
 
 
 def main(_):
+    """
+    Entry point for RL training. Need to specify "gin_file" command line parameter that points to a valid Gin Config.
+    (e.g. "<project-root>/data/configs/dqn.gin")
+    """
     env_name = register_hhh_gym()
 
     logging.set_verbosity(logging.INFO)
@@ -24,15 +29,8 @@ def main(_):
     # activations
     gin.external_configurable(tensorflow.keras.activations.relu, 'tf.keras.activations.relu')
     gin.external_configurable(tensorflow.keras.activations.tanh, 'tf.keras.activations.tanh')
-
-    # sequential model and layers for CNN
-    gin.external_configurable(tensorflow.keras.models.Sequential, 'tf.keras.models.Sequential')
-    gin.external_configurable(tensorflow.keras.layers.Conv2D, 'tf.keras.layers.Conv2D')
-    gin.external_configurable(tensorflow.keras.layers.MaxPool2D, 'tf.keras.layers.MaxPool2D')
-    gin.external_configurable(tensorflow.keras.layers.Flatten, 'tf.keras.layers.Flatten')
-    gin.external_configurable(tensorflow.keras.layers.Dense, 'tf.keras.layers.Dense')
-
     for gin_file in FLAGS.gin_file:
+        gin.add_config_file_search_path(pathlib.Path(gin_file).parent)
         gin.parse_config_file(gin_file)
 
     gin.config._OPERATIVE_CONFIG_LOCK = dask.utils.SerializableLock()  # required for PPO when instantiating envs
